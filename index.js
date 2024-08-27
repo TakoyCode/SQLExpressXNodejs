49f220433d44536bd8e84a75fbda21c01bcb8440
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-08-27 21:41:48 +0200

    Finished all CRUD API calls

--- index.js
+++ index.js
@@ -42,25 +42,41 @@ app.get('/api/users/:id', (req, rep) => {
 app.post('/api/users', (req, rep) => {
     const user = {
         Name: req.body.Name,
-        Age: req.body.Age,
+        Age: parseInt(req.body.Age),
         Email: req.body.Email,
     };
 
     const request = new sql.Request();
-    request.query(`INSERT INTO users (Name, Age, Email) VALUES (${user.Name}, ${user.Age}, ${user.Email})`, (error, result) => {
-        // if (result.rowsAffected <= 0) return rep.status(404).send('Could not find user.');
+    request.query(`INSERT INTO users (Name, Age, Email) VALUES ('${user.Name}', ${user.Age}, '${user.Email}')`, (error, result) => {
 
-        rep.send(result);
+        if (error) rep.status(400).send(error.message);
+        else rep.send(result);
+        console.log(result);
     });
 });
 
-app.put('/api/users', (req, rep) => {
+app.put('/api/users/:id', (req, rep) => {
+    const user = {
+        Name: req.body.Name,
+        Age: parseInt(req.body.Age),
+        Email: req.body.Email,
+    };
 
+    const request = new sql.Request();
+    request.query(`UPDATE users SET Name = '${user.Name}', Age = ${user.Age}, Email = '${user.Email}' Where ID = ${req.params.id};`, (error, result) => {
+        if (error) rep.status(400).send(error.message);
+        else rep.send(result);
+        console.log(result);
+    });
 });
 
-app.delete('/api/users', (req, rep) => {
-
+app.delete('/api/users/:id', (req, rep) => {
+    const request = new sql.Request();
+    request.query(`DELETE FROM users Where ID = ${parseInt(req.params.id)};`, (error, result) => {
+        if (error) rep.status(400).send(error.message);
+        else rep.send(result);
+    });
 });
 
-const port = 3000;
+const port = process.env.PORT | 3000;
 app.listen(port, () => { console.log(`Listening on port ${port}...`) });
