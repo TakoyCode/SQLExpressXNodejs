3db394fae3f5418a56901579da8420956d920968
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-08-29 11:15:11 +0200

    Made the code use less query's

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const Joi = require('joi');
 const sql = require('mssql');
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 const sqlConfig = {
-    user: 'sa',
-    password: 'SQLEXPRESS',
-    database: 'ExpressXNode',
-    server: 'localhost',
+    user: process.env.SQLUSER,
+    password: process.env.SQLPASS,
+    database: process.env.DATABASE,
+    server: process.env.SERVER,
     options: {
         trustServerCertificate: true, //true for local dev / self-signed certs
     }
@@ -32,7 +33,8 @@ app.get('/api/users', (req, res) => {
 app.get('/api/users/:id', (req, res) => {
     const request = new sql.Request();
     request.query(`select * from users where Id = ${req.params.id}`, (error, result) => {
-        if (result.rowsAffected <= 0) return res.status(404).send('Could not find user.');
+        if (error) return res.status(400).send(error.message);
+        if (result.rowsAffected <= 0) return res.status(204).send('Could not find user.');
 
         res.send(result.recordset[0]);
     });
@@ -48,24 +50,12 @@ app.post('/api/users', (req, res) => {
 
     // Create new user in users table
     const request = new sql.Request();
-    request.query(`INSERT INTO users (Name, Age, Email) VALUES ('${Name}', ${Age}, '${Email}'); SELECT SCOPE_IDENTITY() AS ID;`, (error, result) => {
+    request.query(`INSERT INTO users (Name, Age, Email) OUTPUT INSERTED.* VALUES ('${Name}', ${Age}, '${Email}');`, (error, result) => {
         // Checks for error
         if (error) return res.status(400).send(error.message);
 
         // Sends the inputed user back
-        // Usikker om jeg skal skal query databasen for den nye brukeren og sende resultatet av det tilbake,
-        // eller lage en ny variabel og sende den tilbake  
-        request.query(`select * from users where Id = ${result.recordset[0].ID}`, (error, result) => {
-            if (error) return res.status(404).send(error.message);
-            res.send(result.recordset[0]);
-        });
-        // const user = {
-        //     ID: result.recordset[0].ID,
-        //     Name: req.body.Name,
-        //     Age: parseInt(req.body.Age),
-        //     Email: req.body.Email,
-        // };
-        // res.send(user);
+        res.send(result.recordset[0]);
     });
 });
 
@@ -80,18 +70,17 @@ app.put('/api/users/:id', (req, res) => {
     const request = new sql.Request();
     request.query(`select * from users where Id = ${req.params.id}`, (error, result) => {
         // Check if we got a user
-        if (result.rowsAffected <= 0) return res.status(404).send('Could not find user.');
+        if (error) return res.status(400).send(error.message);
+        if (result.rowsAffected <= 0) return res.status(204).send('Could not find user.');
 
         // Update user
-        request.query(`UPDATE users SET Name = '${Name}', Age = ${Age}, Email = '${Email}' Where ID = ${req.params.id};`, (error, result) => {
-            if (error) return res.status(400).send(error.message);
-
-            // Sends updated user back
-            request.query(`select * from users where Id = ${req.params.id}`, (error, result) => {
-                if (error) return res.status(404).send(error.message);
+        request.query(`UPDATE users SET Name = '${Name}', Age = ${Age}, Email = '${Email}' OUTPUT inserted.*, 
+                        inserted.ID as old_ID, deleted.Name as old_Name, deleted.Age as old_Age, deleted.Email as old_Email Where ID = ${req.params.id};`,
+            (error, result) => {
+                if (error) return res.status(400).send(error.message);
+                // Sends updated user back
                 res.send(result.recordset[0]);
             });
-        });
     });
 });
 
@@ -100,15 +89,14 @@ app.delete('/api/users/:id', (req, res) => {
     //Find user
     request.query(`select * from users where Id = ${req.params.id}`, (error, result) => {
         // Check if we got a user
-        if (result.rowsAffected <= 0) return res.status(404).send('Could not find user.');
-        // Save user
-        const user = result.recordset[0];
+        if (error) return res.status(400).send(error.message);
+        if (result.rowsAffected <= 0) return res.status(204).send('Could not find user.');
 
         // Delete user
-        request.query(`DELETE FROM users Where ID = ${parseInt(req.params.id)};`, (error, result) => {
+        request.query(`DELETE FROM users OUTPUT DELETED.* Where ID = ${parseInt(req.params.id)};`, (error, result) => {
             if (error) return res.status(400).send(error.message);
             // Send back deleted user
-            res.send(user);
+            res.send(result.recordset[0]);
         });
     });
 });
@@ -124,4 +112,3 @@ function ValidateUser(user) {
 
 const port = process.env.PORT | 3000;
 app.listen(port, () => { console.log(`Listening on port ${port}...`) });
-
